e20c2004a382bd5c14a97085e0370f99d9f2407b
fahim <fahimahamedweb@gmail.com>
2024-01-08 00:24:19 +0600

    brand api and admin dashboard done

--- reactshop/src/App.js
+++ reactshop/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import AdminDashboard from "./components/Admin/AdminDashboard";
 import HomePage from "./components/HomePage/HomePage";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -8,6 +9,14 @@ const App = () => {
     <BrowserRouter>
     <Navbar />
     <Routes>
+    <Route
+          path="admin"
+          element={
+           
+              <AdminDashboard />
+          
+          }
+        />
     <Route path="/" element={<HomePage />} />
     </Routes>
 </BrowserRouter>

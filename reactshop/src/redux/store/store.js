fc28520d6eb0eca449e4ff96dd94dbd1960cc82d
fahim <fahimahamedweb@gmail.com>
2024-01-16 20:42:01 +0600

    redux added

---
+++ reactshop/src/redux/store/store.js
@@ -0,0 +1,22 @@
+import { configureStore } from "@reduxjs/toolkit";
+import brandsReducer from "../slices/categories/brandsSlice";
+import categoryReducer from "../slices/categories/categoriesSlice";
+import colorsReducer from "../slices/categories/colorsSlice";
+import productReducer from "../slices/products/productSlices";
+
+import usersReducer from "../slices/users/usersSlice";
+
+//store
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+    products: productReducer,
+    categories: categoryReducer,
+    brands: brandsReducer,
+    colors: colorsReducer,
+ 
+ 
+  },
+});
+
+export default store;

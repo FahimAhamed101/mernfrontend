27695bad3cf4b02d74222f71333921307270e746
fahim <fahimahamedweb@gmail.com>
2024-01-18 02:03:48 +0600

    user functionality done

--- reactshop/src/App.js
+++ reactshop/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from "./components/Users/Forms/Login";
+import RegisterForm from "./components/Users/Forms/RegisterForm";
 import AdminDashboard from "./components/Admin/AdminDashboard";
 import HomePage from "./components/HomePage/HomePage";
 import Navbar from "./components/Navbar/Navbar";
+import AdminRoutes from "./components/AuthRoute/AdminRoutes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 const App = () => {
   return (
     <BrowserRouter>
@@ -13,10 +17,14 @@ const App = () => {
           path="admin"
           element={
            
-              <AdminDashboard />
+            <AdminRoutes>
+            <AdminDashboard />
+          </AdminRoutes>
           
           }
         />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterForm />} />
     <Route path="/" element={<HomePage />} />
     </Routes>
 </BrowserRouter>
--- reactshop/src/redux/store/store.js
+++ reactshop/src/redux/store/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "../slices/cart/cartSlices";
 import brandsReducer from "../slices/categories/brandsSlice";
 import categoryReducer from "../slices/categories/categoriesSlice";
 import colorsReducer from "../slices/categories/colorsSlice";
+import couponsReducer from "../slices/coupons/couponsSlice";
+import ordersReducer from "../slices/orders/ordersSlices";
 import productReducer from "../slices/products/productSlices";
-
+import reviewsReducer from "../slices/reviews/reviewsSlice";
 import usersReducer from "../slices/users/usersSlice";
 
 //store
@@ -14,8 +17,10 @@ const store = configureStore({
     categories: categoryReducer,
     brands: brandsReducer,
     colors: colorsReducer,
- 
- 
+    carts: cartReducer,
+    coupons: couponsReducer,
+    orders: ordersReducer,
+    reviews: reviewsReducer,
   },
 });
 

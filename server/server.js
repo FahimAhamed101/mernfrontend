8a8f32e36705d3bd4368200eab128af313624dee
fahim <fahimahamedweb@gmail.com>
2024-01-20 22:11:50 +0600

    cors issue fxed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-import http from "http";
+import express from "express";
 import app from "./app/app.js";
 
 //create the server
 const PORT = process.env.PORT || 2030;
-const server = http.createServer(app);
-server.listen(PORT, console.log(`Server is up and running on port ${PORT}`));
+const app = express();
+app.listen(PORT, console.log(`Server is up and running on port ${PORT}`));

62c0b81b8bc011fb585675345f6a853022c98097
fahim <fahimahamedweb@gmail.com>
2024-01-20 22:04:16 +0600

    cors issue fxed

--- server/app/app.js
+++ server/app/app.js
@@ -21,7 +21,7 @@ dbConnect();
 const app = express();
 //cors
 const corsOptions = {
-  origin: 'https://mernfrontend-sigma.vercel.appl.app',
+  origin: 'https://reactshop-pi.vercel.app/',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
